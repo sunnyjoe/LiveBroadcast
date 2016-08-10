38089c0ad58b1e692699c7c3d46b388181a64f94
Jiao <jiaoqing@mozat.com>
2016-08-10 09:55:24 +0800

    first create the project

---
+++ LiveBroadcast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveBroadcast
+//
+//  Created by Jiao on 10/8/16.
+//  Copyright © 2016 Jiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveBroadcast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveBroadcast
+//
+//  Created by Jiao on 10/8/16.
+//  Copyright © 2016 Jiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
